d84aa958200a96600dd287671893db7cc6f63bd6
Emerson Victor <emersonvvictor@gmail.com>
2018-10-05 09:02:25 -0300

    Create route for tutorials page

--- como-e-que-e/config/routes.rb
+++ como-e-que-e/config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'tutorials/index'
-  get "index/index"
+  get "index/", to: "index#index", as: "index"
+  get "tutoriais/", to: "tutorials#index", as: "tutorials"
+  
   root "index#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
