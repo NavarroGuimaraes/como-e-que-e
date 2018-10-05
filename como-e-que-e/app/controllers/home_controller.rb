2a5fc2533f9d330dbb51c0153d005717c5da231b
jtomaz99 <jtomaz1999@gmail.com>
2018-10-04 22:11:31 -0300

    home-controller-second-version

--- como-e-que-e/app/controllers/home_controller.rb
+++ como-e-que-e/app/controllers/home_controller.rb
@@ -1,18 +1,4 @@
 class HomeController < ActionController::Base
-    def nav_bar
-        link_to 'how_it_works',
-        :onclick => 'how_it_works.html'
-
-        link_to 'tutorials',
-        :onclick => 'tutorials.html'
-
-        link_to 'contact',
-        onclick => 'contact.html'
-
-        link_to 'login',
-        :onclick => 'login.html'
-
-        link_to 'register',
-        onclick => 'register.html'
+    def tutorials
     end
 end
