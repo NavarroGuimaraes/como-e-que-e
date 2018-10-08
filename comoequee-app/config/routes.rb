1dcdcec95f03da5d0635e05161da345c694f577b
Emerson Victor <emersonvvictor@gmail.com>
2018-10-08 19:21:16 -0300

    Fix routes.rb

--- comoequee-app/config/routes.rb
+++ comoequee-app/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "tutoriais/", to: "tutorials#index", as: "tutorials"
   get 'register/user'
   resources :users
-  
+
   root "index#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
