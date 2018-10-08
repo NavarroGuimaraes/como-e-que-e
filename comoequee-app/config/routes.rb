4805341333e8d0d752c8a618878e0464c292ce4a
Navarro A. G. Silva <nags@cin.ufpe.br>
2018-10-07 23:00:02 -0300

    WIP: add register user controller

--- comoequee-app/config/routes.rb
+++ comoequee-app/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'register/user'
   get "index/index"
   root "index#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
