4e0fa62046f089b8778ed64b218c492cd6102d1f
Emerson Victor <emersonvvictor@gmail.com>
2018-10-12 14:35:04 -0300

    Run rubocop and change routes

--- comoequee-app/config/routes.rb
+++ comoequee-app/config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'session/destroy'
-  get 'session/create'
-  get 'session/new'
-  # FORMAT: get: [path], to: [controller#action], as: [name of route]
-  get "index/", to: "index#index", as: "index"
+  get "logout/", to: "session#destroy", as: "logout"
+  get "login/", to: "session#create", as: "login"
+  get "cadastro/", to: "session#new", as: "register"
   get "tutoriais/", to: "tutorials#index", as: "tutorials"
+  get "index/", to: "index#index", as: "index"
 
   root "index#index"
+  # FORMAT: get: [path], to: [controller#action], as: [name of route]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
