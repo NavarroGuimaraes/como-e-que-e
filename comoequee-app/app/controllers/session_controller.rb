72b8a3b319a1a4751875c7ab92b713d7ad3b29d4
Emerson Victor <emersonvvictor@gmail.com>
2018-10-21 14:54:21 -0300

    Create user model and add validation to model

--- comoequee-app/app/controllers/session_controller.rb
+++ comoequee-app/app/controllers/session_controller.rb
@@ -5,6 +5,13 @@ class SessionController < ApplicationController
   end
 
   def create
+    # user = User.new(user_params)
+    # if user.save
+    #   session[:user_id] = user.id
+    #   redirect_to '/'
+    # else
+    #   redirect_to '/signup'
+    # end
   end
 
   def destroy
---
+++ comoequee-app/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Validation
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, 'valid_email_2/email': true
+  has_secure_password
+
+end
