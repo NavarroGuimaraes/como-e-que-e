8f8360b4f60cf8f5e41998c994e1a6c34bc406e7
Emerson Victor <emersonvvictor@gmail.com>
2018-10-21 22:01:40 -0300

    Refactor register and login controllers

--- comoequee-app/app/controllers/profile_controller.rb
+++ comoequee-app/app/controllers/profile_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ProfileController < ApplicationController
-  before_filter :authorize
+  before_action :authorize
 
   def index
   end
--- comoequee-app/app/controllers/user_controller.rb
+++ comoequee-app/app/controllers/user_controller.rb
@@ -6,16 +6,18 @@ class UserController < ApplicationController
 
   def create
     user = User.new(user_params)
+
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to index_url, notice: 'Cadastro realizado com sucesso!'
     else
-      redirect_to '/'
+      flash[:error] = 'Ops, parece que ocorreu um erro :( Tente novamente'
+      redirect_to login_url
     end
   end
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :phone, :facebook, :bio, :password, :password_confirmation)
     end
 end
