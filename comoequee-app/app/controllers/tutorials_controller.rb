1e379f8c8b4053089a46ede48817e562384fd9a6
Emerson Victor <emersonvvictor@gmail.com>
2018-10-27 11:54:55 -0300

    Add CSS to view

--- comoequee-app/app/controllers/tutorials_controller.rb
+++ comoequee-app/app/controllers/tutorials_controller.rb
@@ -2,6 +2,6 @@
 
 class TutorialsController < ApplicationController
   def index
-    @tutorials = Tutorial.order(:name).paginate(page: params[:page], per_page: 12)
+    @tutorials = Tutorial.order(:name).paginate(page: params[:page], per_page: 1)
   end
 end
