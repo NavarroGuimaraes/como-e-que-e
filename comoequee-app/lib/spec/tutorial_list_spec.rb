c7d4ceaa3b2cb8c98af255f9ed09fa6da1a4d654
jtomaz99 <jtomaz1999@gmail.com>
2018-10-27 09:40:49 -0300

    new tests files with respec

---
+++ comoequee-app/lib/spec/tutorial_list_spec.rb
@@ -0,0 +1,8 @@
+describe TutorialList do
+    context 'get tutorial' do
+        it 'get tutorial list' do
+            test1 = TutorialList.new
+            expect(test1.TutorialList()).to eql (!= Null)
+        end
+    end
+    
