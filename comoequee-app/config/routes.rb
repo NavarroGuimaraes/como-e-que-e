3de770ab948bb40aceafa631c089943fe98dddb6
Emerson Victor <emersonvvictor@gmail.com>
2018-11-17 11:55:36 -0300

    Create routes and controller for tutorial

--- comoequee-app/config/routes.rb
+++ comoequee-app/config/routes.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'tutorial/index'
   # Session and user routes
-  get  '/login/',                 to: 'session#new',          as: 'login'
-  post '/login/',                 to: 'session#create',       as: 'login-form'
-  get  '/logout/',                to: 'session#destroy',      as: 'logout'
-  get  '/cadastro/',              to: 'user#new',             as: 'register'
-  post '/cadastro/',              to: 'user#create',          as: 'register-form'
-  get  '/delete/',                to: 'user#destroy',         as: 'delete-user'
+  get  '/login/',                     to: 'session#new',          as: 'login'
+  post '/login/',                     to: 'session#create',       as: 'login-form'
+  get  '/logout/',                    to: 'session#destroy',      as: 'logout'
+  get  '/cadastro/',                  to: 'user#new',             as: 'register'
+  post '/cadastro/',                  to: 'user#create',          as: 'register-form'
+  get  '/delete/',                    to: 'user#destroy',         as: 'delete-user'
   # Login with google
-  get 'auth/:provider/callback',  to: 'session#createGoogle', as: 'login-google'
-  get 'auth/failure',             to: redirect('/login/'),    as: 'login-failure'
+  get 'auth/:provider/callback',      to: 'session#createGoogle', as: 'login-google'
+  get 'auth/failure',                 to: redirect('/login/'),    as: 'login-failure'
+  # Tutorials
+  get  '/tuto/:tutorial_id',          to: 'tutorial#show',        as: 'first-tutorial',   constraints: { tutorial_id: /.*/ }
+  get  '/tutorial/:tutorial_id/:chapter_id/:content_id',          to: 'tutorial#index',       as: 'tutorial',         constraints: { tutorial_id: /.*/, chapter_id: /.*/, content_id: /.*/  }
   # Main routes
-  get  '/perfil/',                to: 'profile#index',        as: 'profile'
-  get  '/tutoriais/',             to: 'tutorials#index',      as: 'tutorials'
-  get  '/index/',                 to: 'index#index',          as: 'index'
+  get  '/perfil/',                    to: 'profile#index',        as: 'profile'
+  get  '/tutoriais/',                 to: 'tutorials#index',      as: 'tutorials'
+  get  '/index/',                     to: 'index#index',          as: 'index'
 
   root 'index#index'
   # FORMAT: get: [path], to: [controller#action], as: [name of route]
