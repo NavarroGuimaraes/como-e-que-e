fe7c872830559d5325012d82173faa3c7279270f
Emerson Victor <emersonvvictor@gmail.com>
2018-11-25 18:58:49 -0300

    Closes #156

--- comoequee-app/db/schema.rb
+++ comoequee-app/db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2018_11_26_021827) do
+=======
+ActiveRecord::Schema.define(version: 2018_11_25_211100) do
+>>>>>>> Closes #156
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- comoequee-app/lib/spec/ruby.rb
+++ comoequee-app/lib/spec/ruby.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'spec'
 
 if Spec::Ruby.version.to_f >= 1.9
-  gem 'test-unit','= 1.2.3'
+  gem 'test-unit', '= 1.2.3'
 end
 
 require 'test/unit'
@@ -16,7 +18,7 @@ Loaded:   test-unit-#{Test::Unit::VERSION}
 With ruby-1.9, rspec-#{Spec::VERSION::STRING} requires test-unit-1.2.3, and
 tries to force it with "gem 'test-unit', '= 1.2.3'" in:
   #{__FILE__}
-  
+
 Unfortunately, test-unit-#{Test::Unit::VERSION} was loaded anyway. While we are
 aware of this bug we have not been able to track down its source.
 Until we do, you have two alternatives:
