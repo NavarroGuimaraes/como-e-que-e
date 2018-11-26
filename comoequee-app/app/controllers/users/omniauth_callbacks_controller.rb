b2d66315b3089a1d1a8ea2654eff6045e21adeff
Jose Neto <jcsn2@cin.ufpe.br>
2018-11-26 21:52:32 +0000

    Finalização do login com Facebook, redirecionamento correto

--- comoequee-app/app/controllers/users/omniauth_callbacks_controller.rb
+++ comoequee-app/app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,6 +6,7 @@ module Users
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication # Throws if @user is not activated
         set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+        session[:user_id] = @user.id
       else
         session['devise.facebook_data'] = request.env['omniauth.auth']
         redirect_to index_url
